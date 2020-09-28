2e4137a91a933ba7d1c2910f7063b2467ee9ab28
PatrickHarding <patoro7247@gmail.com>
2020-09-27 21:50:55 -0700

    s is First upload of ThoughtJot 9/27/2020

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+print("Before we get started, what's your name?")
+name = input()
+
+name = name.capitalize()
+
+print("Hi, "+name+"\n")
+
+time.sleep(3)
+
+print("Welcome to ThoughtJot, please jot down any thoughts you have today to be recorded.\nIf yoo want to "+
+      "recollect something from a specific date, please type recollect, followed by the date")
+
+
+
